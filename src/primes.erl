2b22babb5a98f02e2b03d783f2c694ef1c457207
Martin <martinjonsson01@gmail.com>
2022-02-05 20:21:15 +0100

    Implement advanced sieve

--- src/primes.erl
+++ src/primes.erl
@@ -13,18 +13,76 @@
 
 calculate(N) ->
   Pid = self(),
+  Start = erlang:timestamp(),
   spawn(fun() -> sieve(N, Pid) end),
   receive
-    {done, Primes} -> Primes
+    {done, Primes} ->
+      End = erlang:timestamp(),
+      TDiff = timer:now_diff(End, Start),
+      io:format("Calculated ~p primes in ~p seconds:\n", [length(Primes), TDiff / 100000]),
+      io:format("~w\n", [Primes])
   end.
 
-sieve(N, Callback) ->
-  sieve(Callback, lists:seq(2, N), []).
 
-sieve(Callback, [], Primes) ->
-  Callback ! {done, Primes};
-sieve(Callback, [N | Candidates], Primes) ->
-  io:format("checking ~p out of ~p candidates, up to ~p primes ~p\n", [N, length(Candidates), length(Primes), Primes]),
-  NewPrimes = Primes ++ [N],
-  NewCandidates = [P || P <- Candidates, P rem N =/= 0],
-  sieve(Callback, NewCandidates, NewPrimes).
+sieve(N, Callback) ->
+  % Place not_prime instead of 1 so indices match the numbers.
+  sieve(Callback, N, 2, [not_prime] ++ lists:seq(2, N)).
+
+sieve(Callback, N, Prime, Marked) ->
+  NewMarked = mark_multiples_of(Prime, N, Marked),
+  {_, [_ | UpcomingUnmarked]} = lists:split(Prime - 1, Marked), % Only use marked elements coming after current prime
+  NextPrime = next_unmarked(UpcomingUnmarked),
+  NLimit = math:sqrt(N),
+  if
+    NextPrime > NLimit -> % Stop early if above sqrt(N)
+      respond_with_primes(Callback, Marked);
+    true ->
+      case NextPrime of
+        no_unmarked -> respond_with_primes(Callback, Marked);
+        _ -> sieve(Callback, N, NextPrime, NewMarked)
+      end
+  end.
+
+
+mark_multiples_of(N, Limit, Marked) ->
+  io:fwrite("N is ~p with limit ~p\n", [N, Limit]),
+  mark_multiples_of(N, N + N, Limit, Marked).
+
+mark_multiples_of(_, Index, Limit, Marked) when Index > Limit ->
+  Marked;
+mark_multiples_of(N, Index, Limit, Marked) ->
+  {Left, [_ | Right]} = lists:split(Index - 1, Marked),
+  NewMarked = Left ++ [not_prime] ++ Right,
+  %io:fwrite("index is ~p marked becomes ~p\n", [Index, NewMarked]),
+  %timer:sleep(100),
+  mark_multiples_of(N, Index + N, Limit, NewMarked).
+
+next_unmarked([]) ->
+  no_unmarked;
+next_unmarked([H | T]) ->
+  case H of
+    not_prime -> next_unmarked(T);
+    Unmarked -> Unmarked
+  end.
+
+
+respond_with_primes(Callback, Marked) ->
+  Primes = all_unmarked(Marked),
+  Callback ! {done, Primes}.
+
+all_unmarked(Marked) ->
+  First = next_unmarked(Marked),
+  all_unmarked(First, Marked, [First]).
+
+all_unmarked(Current, Marked, Unmarked) ->
+  {_, [_ | UpcomingMarked]} = lists:split(Current - 1, Marked), % Only use marked elements coming after current
+  Next = next_unmarked(UpcomingMarked),
+  case Next of
+    no_unmarked ->
+      Unmarked;
+    _ ->
+      NewUnmarked = Unmarked ++ [Next],
+      all_unmarked(Next, Marked, NewUnmarked)
+  end.
+
+
